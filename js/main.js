07b652ba5a1e79f9298d6c33f9d14f722806d392
AmarDavda <amardavda1715@gmail.com>
2026-03-19 00:26:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -350,7 +350,8 @@ function loadContactPage() {
             }
 
             if (item.title === "Email") {
-                link = `mailto:${item.content}?subject=Hello Amar`;
+                link = `mailto:${item.content}?subject=Contact%20from%20Portfolio&body=Hello%2C%0AI%20visited%20your%20portfolio%20and%20would%20like%20to%20connect%20with%20you.%0A%0AThanks">
+ `;
             }
 
             if (item.title === "Address") {
@@ -360,7 +361,7 @@ function loadContactPage() {
             if (item.title === "Whatsapp") {
     if (item.content) {
         const number = item.content;
-        link = `https://wa.me/${number}?text=Hello Amar,`;
+        link = `https://wa.me/${number}?text=Hello Amar, I connect you from your Portfolio!`;
     } else {
         link = "#"; // no number available
     }
