4dfe59708b24a563e39c450d8d6fefcf29af178b
AmarDavda <amardavda1715@gmail.com>
2026-03-19 00:49:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -350,7 +350,7 @@ function loadContactPage() {
             }
 
             if (item.title === "Email") {
-                link = `mailto:${item.content}?subject=Contact%20from%20Portfolio&body=Hello%2C%0AI%20visited%20your%20portfolio%20and%20would%20like%20to%20connect%20with%20you.%0A%0AThanks">
+                link = `mailto:${item.content}?subject=Contact%20from%20Portfolio&body=Hello%2C%0AI%20visited%20your%20portfolio%20and%20would%20like%20to%20connect%20with%20you.%0A%0AThanks
  `;
             }
 
